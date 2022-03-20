9ada3bdbef7fd5b125a44927de2119d7d009a685
prasad <prasadanduri13gmail.com>
2022-03-20 19:36:23 +0530

    Fetch data for rice

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InventoryDataManagement
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = C:\Users\Prasad\source\repos\InventoryDataManagement\InventoryDataManagement\Rice.json;
+
+            FetchDataForRice fetchDataForRice = new FetchDataForRice();
+            Rice data = fetchDataForRice.Read(path);
+
+            Console.WriteLine(data.Name);
+            Console.WriteLine(data.Weight);
+            Console.WriteLine(data.Price);
+
+
+        }
+    }
+}
